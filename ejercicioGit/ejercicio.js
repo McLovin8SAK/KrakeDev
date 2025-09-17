86d71afd5cfc3f3fcaf9e78cecf87e47be85336c
McLovin8SAK <crisdan.metal277@gmail.com>
2025-09-17 13:00:43 -0500

    el botón cambia la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/srBurns.gif"
     console.log("Ingresa a saludar");
+
 }
