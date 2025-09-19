3f916f62ee75dff82c8c4d2340d4b52f643b446a
McLovin8SAK <crisdan.metal277@gmail.com>
2025-09-19 11:10:01 -0500

    función limpiar añadida correctamente

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -29,13 +29,16 @@ calcularValorTotal = function () {
     mostrarTexto("lblTotal",valorTotal.toFixed(2));
             
     mostrarTexto("lblResumen","Valor a pagar por "+cantidad+" "+nombreProducto+" con "+porcentajeDescuento+"% de descuento es "+valorTotal);
-
 }
 limpiar = function () {
-    /*
-        Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
-        Dejar todos los textos de los montos con el valor 0.0
-        Si funciona, hacer un commit
-     */
+    mostrarTextoEnCaja("txtProducto","");
+    mostrarTextoEnCaja("txtPrecio","");
+    mostrarTextoEnCaja("txtCantidad","");
+    mostrarTextoEnCaja("txtPorcentajeDescuento","");
+
+    mostrarTexto("lblSubtotal","0.0");
+    mostrarTexto("lblDescuento","0.0");
+    mostrarTexto("lblValorIVA","0.0");
+    mostrarTexto("lblTotal", "0.0");
+    mostrarTexto("lblResumen", "");
 }
-/* SI TODO FUNCIONA, HACER UN PUSH */
