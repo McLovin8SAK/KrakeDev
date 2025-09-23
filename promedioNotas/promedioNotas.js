5dd315c9e598c47e76a164ce6a466bb6e5ab3f2c
McLovin8SAK <crisdan.metal277@gmail.com>
2025-09-23 10:50:38 -0500

    Reto32: implementadas los condicionales y operadores lógicos

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,9 +5,17 @@ calcularPromedioNotas=function(){
 
     let promedio=calcularPromedio(nota1,nota2,nota3);
     mostrarTexto("lblResultado",promedio.toFixed(2));
-    if(promedio>7){
-        mostrarImagen("lblImagen","cartman.gif");
-    }else{
+    if(promedio<5 && promedio>0){
+        mostrarTexto("lblAprobacion", "REPROBADO");
         mostrarImagen("lblImagen","cartmanEnojado.gif");
+    }else if(promedio>=5 && promedio<=8){
+        mostrarTexto("lblAprobacion", "BUEN TRABAJO");
+        mostrarImagen("lblImagen","cartman.gif");
+    }else if(promedio>8 && promedio<=10){
+        mostrarTexto("lblAprobacion","EXCELENTE");
+        mostrarImagen("lblImagen", "cartmanCool.gif");
+    }else{
+        mostrarTexto("lblAprobacion", "DATOS INCORRECTOS");
+        mostrarImagen("lblImagen", "cartmanNo.gif");
     }
 }
