44202480bc4b7db9eb12d266c5cf1852ebc46fbc
McLovin8SAK <crisdan.metal277@gmail.com>
2025-09-30 17:34:46 -0500

    Reto40: paso 6 completado

--- juego_ahorcado/ahorcado.js
+++ juego_ahorcado/ahorcado.js
@@ -55,6 +55,7 @@ validar = function (letra) {
     if (letrasEncontradas==0) {
         alert("LA LETRA NO ES PARTE DE LA PALABRA");
         errores++;
+        mostrarAhorcado();
     }
 }
 
@@ -71,5 +72,35 @@ ingresarLetra = function () {
         }
     } else {
         alert("SOLO SE ACEPTAN MAYUSCULAS");
+    }
+}
+
+mostrarAhorcado=function(){
+    if(errores==1){
+        mostrarImagen("ahorcadoImagen","Ahorcado_01.png");
+    }
+    if(errores==2){
+        mostrarImagen("ahorcadoImagen","Ahorcado_02.png");
+    }
+    if(errores==3){
+        mostrarImagen("ahorcadoImagen","Ahorcado_03.png");
+    }
+    if(errores==4){
+        mostrarImagen("ahorcadoImagen","Ahorcado_04.png");
+    }
+    if(errores==5){
+        mostrarImagen("ahorcadoImagen","Ahorcado_05.png");
+    }
+    if(errores==6){
+        mostrarImagen("ahorcadoImagen","Ahorcado_06.png");
+    }
+    if(errores==7){
+        mostrarImagen("ahorcadoImagen","Ahorcado_07.png");
+    }
+    if(errores==8){
+        mostrarImagen("ahorcadoImagen","Ahorcado_08.png");
+    }
+    if(errores==9){
+        mostrarImagen("ahorcadoImagen","Ahorcado_09.png");
     }
 }
