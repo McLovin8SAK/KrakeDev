f346eaea82ea9762723394a8e1c8ffefdad5a24d
McLovin8SAK <crisdan.metal277@gmail.com>
2025-10-02 14:00:27 -0500

    Reto42: tabla randómica completada

--- arreglos/ejercicios1.js
+++ arreglos/ejercicios1.js
@@ -24,6 +24,7 @@ probarAgregar = function () {
 
 agregarNota = function (nota) {
     notas.push(nota);
+    mostrarNotas();
 }
 
 
@@ -31,13 +32,34 @@ calcularPromedio = function () {
     let sumaNotas = 0;
     let promedio;
     for (let indice = 0; indice < notas.length; indice++) {
-        sumaNotas+=notas[indice];
+        sumaNotas += notas[indice];
     }
-    promedio=sumaNotas/notas.length;
+    promedio = sumaNotas / notas.length;
     return promedio;
 }
 
-ejecutarPromedio=function(){
-    let promedioCalculado=calcularPromedio();
+ejecutarPromedio = function () {
+    let promedioCalculado = calcularPromedio();
     mostrarTexto("lblPromedio", promedioCalculado);
+}
+
+generarTabla = function () {
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla += "<table><tr><td>UNO</td></tr>" + "<tr><td>DOS</td></tr></table>";
+    cmpTabla.innerHTML = contenidoTabla;
+}
+
+mostrarNotas = function () {
+    let cmpTabla = document.getElementById("divTabla");
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let miNota;
+    for (let i = 0; i < notas.length; i++) {
+        miNota = notas[i];
+        contenidoTabla += "<tr><td>";
+        contenidoTabla += miNota;
+        contenidoTabla += "</tr></td>";
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
